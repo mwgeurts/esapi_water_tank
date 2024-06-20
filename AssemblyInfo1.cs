695e9c569ee812d13b5fbf7d5edd95be39f508c4
Mark Geurts <mark.w.geurts@gmail.com>
2024-06-20 14:13:11 -0500

    Added AssemblyInfo

---
+++ AssemblyInfo1.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProfileComparison")]
+[assembly: AssemblyDescription("Varian Eclipse ESAPI plugin to compare dose to water tank profiles")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aspirus Health")]
+[assembly: AssemblyProduct("ProfileComparison")]
+[assembly: AssemblyCopyright("Mark Geurts, Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bcbc6d1a-424a-4819-8d4f-63c932214c13")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
